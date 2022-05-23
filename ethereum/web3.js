5373d2b0ec5d565f81a94bce58ff2ff8c9603c42
nightsyber25 <preem.2010@hotmail.com>
2022-05-23 13:43:02 +0700

    Lastest (Lec 182)

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -3,9 +3,10 @@ import Web3 from 'web3';
 let web3;
 
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
-    web3 = new Weeb3(window.ethereum);
+    window.ethereum.request({ method: "eth_requestAccounts" });
+    web3 = new Web3(window.ethereum);
 } else {
-    const provider = new Web3.provider.HttpProvider(
+    const provider = new Web3.providers.HttpProvider(
         "https://rinkeby.infura.io/v3/17a2ab0671e44a179ea0f44c43ddfa8f"
     );
     web3 = new Web3(provider);
